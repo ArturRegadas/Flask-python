ee1f60c264336e4a9aec7047e4ff1b9c69df2a0c
ArturRegadas <ahlregadas@gmail.com>
2024-11-05 23:03:56 -0300

    architecture and style

--- TasksCheck/request/InDocument/src/script/clientAPI.js
+++ TasksCheck/request/InDocument/src/script/clientAPI.js
@@ -1,3 +1,4 @@
+
 class Client{
 
     constructor(url){
@@ -14,32 +15,30 @@ class Client{
         await fetch(this.url, {
             method: "POST",
             headers: {
-                "Content-Type": "application/json" // Corrigido
+                "Content-Type": "application/json"
             },
-            body: JSON.stringify(data) // Corrigido
+            body: JSON.stringify(data)
         });
     }
 
     async removeTask(){
-        let taskToRemove = document.getElementById("taskToRemove").value
-        let data = {"remove": taskToRemove}
-        console.log(data)
+        let taskToRemove = document.getElementById("taskToRemove").value;
+        let data = {"remove": taskToRemove};
         await fetch(this.url, {
             method: "POST",
             headers: {
-                "Content-Type": "application/json" // Corrigido
+                "Content-Type": "application/json" 
             },
-            body: JSON.stringify(data) // Corrigido
+            body: JSON.stringify(data)
         });
     }
 
     async printTasks(){
         const response = await fetch(this.url);
         const tasks = JSON.stringify(await response.json());
-        document.getElementById("tasks").innerText = tasks
+        console.log(tasks)
     }
 }
 
 const client = new Client("http://127.0.0.1:8080/tasks");
-
 
